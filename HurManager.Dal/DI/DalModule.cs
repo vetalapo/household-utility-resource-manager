b4b7f918b4c3221c1e274f8a00649a1bf83a4219
Vitaly Dushkin <vetalapo@gmail.com>
2021-03-27 16:40:56 +0300

    DI and Mapping sketch

---
+++ HurManager.Dal/DI/DalModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using AutoMapper;
+using HurManager.Core.Services.Session;
+using HurManager.Dal.Context;
+using HurManager.Dal.Mapping;
+
+namespace HurManager.Dal.DI
+{
+    public class DalModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Assembly types
+            builder.RegisterAssemblyTypes(this.ThisAssembly).AsImplementedInterfaces();
+
+            // Mapper maps
+            builder.RegisterType<DalMappingProfile>().As<Profile>();
+
+            // DB context
+            builder.RegisterType<HurManagerContext>().AsSelf();
+
+            // Session and its provider
+            builder.RegisterType<SessionProvider>().As<ISessionProvider>().InstancePerLifetimeScope();
+            builder.Register(ctx => ctx.Resolve<ISessionProvider>().CurrentSession).As<ISession>();
+        }
+    }
+}
---
+++ HurManager.Dal/Mapping/DalMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace HurManager.Dal.Mapping
+{
+    internal class DalMappingProfile : Profile
+    {
+        public DalMappingProfile()
+        {
+        }
+    }
+}

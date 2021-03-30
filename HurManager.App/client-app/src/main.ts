b5221f8291b3a7323783f3c22945be34c1c155c0
Vitaly Dushkin <vetalapo@gmail.com>
2021-03-30 12:48:14 +0300

    Working Vue and Vuetify

--- HurManager.App/client-app/src/main.ts
+++ HurManager.App/client-app/src/main.ts
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify, {
+  iconfont: "mdi",
+});
+
 new Vue({
   router,
   store,
+  vuetify : new Vuetify(),
   render: h => h(App)
 }).$mount('#app')
--- HurManager.App/client-app/src/views/Home.vue
+++ HurManager.App/client-app/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+      <v-app>
+          <v-main>
+              <v-container fluid>
+                <House msg="Welcome to Your Vue.js + TypeScript App"/>
+              </v-container>
+          </v-main>
+      </v-app>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    import { Component, Vue } from 'vue-property-decorator';
+    import House from '@/components/House.vue'; // @ is an alias to /src
+    
+    @Component({
+      components: {
+        House,
+      },
+    })
+    export default class Home extends Vue {
+    }
 </script>
